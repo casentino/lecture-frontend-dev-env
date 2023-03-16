b1b1b54a774a0233dab9588c746c46de11a50e97
casentino <opterk@gmail.com>
2023-03-16 12:09:41 +0900

    웹팩 로더 실습

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,38 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    main: './src/app.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve('./dist'),
-  }
-}
+	mode: 'development',
+	entry: {
+		main: './src/app.js',
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve('./dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(jpg|png|svg|gif)$/,
+				loader: 'url-loader',
+				options: {
+					publicPath: './dist',
+          name: '[name].[ext]?[hash]',
+          limit: 10000
+				},
+			},
+			// {
+			// 	test: /\.(jpg|png|svg|gif)$/,
+			// 	loader: 'file-loader',
+			// 	options: {
+			// 		publicPath: './dist/images',
+			// 		outputPath: './images',
+			// 		name: '[name].[ext]?[hash]',
+			// 	},
+			// },
+		],
+	},
+};
